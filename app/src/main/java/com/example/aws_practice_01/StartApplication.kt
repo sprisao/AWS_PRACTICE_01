1f7adde036f79d8217f30b07a3a09636906f6299
sprisao <whdtmek@gmail.com>
2023-01-19 17:05:08 +0900

    Timber 설치

--- app/src/main/java/com/example/aws_practice_01/StartApplication.kt
+++ app/src/main/java/com/example/aws_practice_01/StartApplication.kt
@@ -2,12 +2,14 @@ package com.example.aws_practice_01
 
 import android.app.Application
 import com.example.aws_practice_01.backend.Backend
+import timber.log.Timber
 
 
 class StartApplication : Application(){
     override fun onCreate() {
         super.onCreate()
         Backend.initialize(applicationContext)
+        Timber.plant(Timber.DebugTree())
     }
 
 }

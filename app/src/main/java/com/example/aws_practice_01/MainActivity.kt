119575363c7a7719fa3afe41a77a79cb0a91c0fc
sprisao <whdtmek@gmail.com>
2023-01-19 17:04:57 +0900

    사용자 인터페이스 코드 업데이트

--- app/src/main/java/com/example/aws_practice_01/MainActivity.kt
+++ app/src/main/java/com/example/aws_practice_01/MainActivity.kt
@@ -5,9 +5,14 @@ import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.RecyclerView
+import com.example.aws_practice_01.backend.Backend
 import com.example.aws_practice_01.data.model.UserData
 import com.example.aws_practice_01.ui.adapter.NoteRecyclerViewAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import kotlinx.android.synthetic.main.content_main.item_list
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +23,18 @@ class MainActivity : AppCompatActivity() {
 
         // prepare our List view and RecyclerView (cells)
         setupRecyclerView(item_list)
+
+        setupAuthButton(UserData)
+
+        UserData.isSignedIn.observe(this, Observer<Boolean> {isSignedUp ->
+
+            Timber.i("User signed in: $isSignedUp")
+            if (isSignedUp) {
+                fabAuth.setImageResource(R.drawable.baseline_lock_open_24)
+            }else {
+                fabAuth.setImageResource(R.drawable.ic_baseline_lock)
+            }
+        })
     }
 
     // recycler view is the list of cells
@@ -30,6 +47,21 @@ class MainActivity : AppCompatActivity() {
             // let's create a RecyclerViewAdapter that manages the individual cells
             recyclerView?.adapter = NoteRecyclerViewAdapter(notes)
         })
+    }
+
+    private fun setupAuthButton(userData: UserData){
+        // register a click listener
+        fabAuth.setOnClickListener { view ->
+            val authButton = view as FloatingActionButton
+
+            if (userData.isSignedIn.value!!){
+                authButton.setImageResource(R.drawable.baseline_lock_open_24)
+                Backend.signOut()
+            } else {
+                authButton.setImageResource(R.drawable.ic_baseline_lock)
+                Backend.signIn(this)
+            }
+        }
     }
 
     companion object {
